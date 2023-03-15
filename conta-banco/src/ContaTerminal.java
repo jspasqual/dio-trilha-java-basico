5944e4d3b7bca58bf8c4e286d2ff864f06629312
jspasqual <js_pasqual@yahoo.com.br>
2023-03-15 18:41:45 -0300

    programa ok

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da agência");
+        int agencia = scanner.nextInt();
+        
+        System.out.println("Por favor, digite o código da conta");
+        String conta = scanner.next();
+
+        System.out.println("Por favor, digite seu nome completo");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, digite seu saldo");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
